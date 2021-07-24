e62a4e7ac34bf2694f6043eab3e028ce3b2d1882
Rafael Bandeira dos Santos <rafael.bandeiradossantos-ext@ab-inbev.com>
2021-07-23 21:59:15 -0300

    double array

---
+++ src/main/kotlin/com/rafaelds/collections/TesteDoubleArray.kt
@@ -0,0 +1,20 @@
+package com.rafaelds.collections
+
+fun main() {
+    val salaries = DoubleArray(3)
+    val remuneracao = doubleArrayOf(1500.0, 2500.0, 3500.0, 4500.0, 8500.0, 9500.0)
+
+    salaries[0] = 2000.0
+    salaries[1] = 3500.0
+    salaries[2] = 4500.0
+
+    salaries.forEachIndexed { index, salary ->
+        salaries[index] = salary * 1.83
+    }
+
+    salaries.forEach { println(it) }
+
+    remuneracao.sort()
+    remuneracao.forEach { println(it) }
+
+}
