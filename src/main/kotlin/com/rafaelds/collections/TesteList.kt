3202c215190395c6f8fca1b1abd1ed83f60757da
Rafael Bandeira dos Santos <rafael.bandeiradossantos-ext@ab-inbev.com>
2021-07-29 19:12:42 -0300

    working with lists

---
+++ src/main/kotlin/com/rafaelds/collections/TesteList.kt
@@ -0,0 +1,22 @@
+package com.rafaelds.collections
+
+fun main() {
+    val rafael = Funcionarios("Rafael", 955.0)
+    val wesley = Funcionarios("Wesley", 610.0)
+    val tiago = Funcionarios("Tiago", 142.0)
+    val funcionarios = listOf<Funcionarios>(rafael, wesley, tiago)
+
+    funcionarios.forEach { println(it.toString()) }
+    println(funcionarios.find { it.nome == "Renata" })
+}
+
+data class Funcionarios(
+    val nome: String,
+    val salario: Double
+) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salario: $salario
+        """.trimIndent()
+}
